cb96a39c305d7eab38d17dcfc2c9662f67a6dbe0
Dora9912 <13779598117@163.com>
2025-06-04 23:15:48 +0800

    Update home.py

--- page_content/home.py
+++ page_content/home.py
@@ -6,17 +6,17 @@ def home_page():
     left_col, right_col = st.columns(2)
     left_col.markdown(
         """
-        <h4>Sarah Johnson</h4>
+        <h4>Ran Ding</h4>
         <p>Recent Master's Graduate in Marketing<br>
         Chinese University of Hong Kong<br>
-        12 Chak Cheung St., Ma Liu Shui, HKSAR<br>
-        <a href="mailto:sarah.johnson@example.com">sarah.johnson@example.com</a></p>
+        H 7/H Tower 7 The Regent, Tai Po, HK<br>
+        <a href="mailto:13779598117@163.com">13779598117@163.com</a></p>
         """,
         unsafe_allow_html=True
     )
 
     # add a photo to the right column
-    image_path = os.path.join("static", "images", "image.png")
+    image_path = os.path.join("static", "images", "image2.jpg")
     if os.path.exists(image_path):
         image = Image.open(image_path)
         right_col.image(image, width=200)
@@ -28,11 +28,11 @@ def home_page():
     st.markdown(
         """
         ### About Me
-        I am a recent master's graduate in Data Science from the University of XYZ, eager to apply my knowledge and skills in a professional setting. During my academic journey, I developed a strong foundation in statistical analysis, machine learning, and data visualization.
+        Hi !  I'm Ding Ran. I'm currently studying for my Master's in Marketing at CUHK, and I graduated from Sichuan University majored in Exhibition Management.
+        
+        Through my internships, I've gotten real hands-on experience in digital marketing and e-commerce.
 
-        As part of my master's program, I completed several projects that involved working with real-world datasets and applying various data science techniques. These projects allowed me to gain hands-on experience in data preprocessing, exploratory data analysis, model building, and evaluation.
-
-        I am passionate about leveraging data to drive insights and make informed decisions. I am a quick learner, a collaborative team player, and possess strong problem-solving skills. I am excited to contribute my skills and grow as a data science professional in a dynamic and challenging environment.
+        Moving forward, I aspire to build a career in the e-commerce field and aim to achieve breakthroughs in this industry!  
         """
     )
 
@@ -41,11 +41,7 @@ def home_page():
         ### Skills
         - Programming Languages: Python, R
         - Data Analysis: Pandas, NumPy, Matplotlib, Seaborn
-        - Machine Learning: Scikit-learn, TensorFlow, Keras
-        - Database: SQL, MongoDB
-        - Data Visualization: Tableau, Power BI
-        - Statistical Analysis: Hypothesis Testing, Regression Analysis
-        - Communication: Presentation Skills, Technical Writing
+        - Work Skill: Word, Powerpoint, Excel, Pr, Ps
         """
     )
 
