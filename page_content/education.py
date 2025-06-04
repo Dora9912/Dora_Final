afd8e9f3105c4f11670f482db2703fde6a313243
Dora9912 <13779598117@163.com>
2025-06-04 23:37:32 +0800

    Update education.py

--- page_content/education.py
+++ page_content/education.py
@@ -19,48 +19,13 @@ def education_page():
     
     st.markdown("---")
     
-    st.markdown("## Certifications")
-    
-    cert1, cert2, cert3 = st.columns(3)
-    
-    with cert1:
-        st.markdown("""
-        ### AWS Certified Data Analytics - Specialty
-        **Amazon Web Services** | *March 2022*
-        
-        Demonstrated expertise in designing, building, securing, and maintaining analytics solutions on AWS.
-        """)
-        
-    with cert2:
-        st.markdown("""
-        ### TensorFlow Developer Certificate
-        **Google** | *January 2022*
-        
-        Validated ability to develop deep learning models using TensorFlow.
-        """)
-        
-    with cert3:
-        st.markdown("""
-        ### Microsoft Certified: Azure Data Scientist Associate
-        **Microsoft** | *November 2021*
-        
-        Demonstrated expertise in using Azure services to train, evaluate, and deploy machine learning models.
-        """)
-    
-    st.markdown("---")
     
     st.markdown("## Academic Projects")
     
     st.markdown("""
-    ### Sentiment Analysis of Product Reviews
-    - Developed a deep learning model to analyze customer reviews and predict sentiment
-    - Achieved 92% accuracy using BERT and fine-tuning techniques
-    - Implemented the model as a web application using Flask
-    
-    ### Image Classification for Medical Diagnosis
-    - Created a convolutional neural network to classify medical images
-    - Worked with a dataset of X-ray images to detect pneumonia
-    - Achieved 88% accuracy and deployed the model on a cloud platform
+    ### Research on the influence factors of the distribution of travel time for the elderly
+    - As the group leader, I led my team finish the project "Research on the influence factors of the distribution of travel time for the elderly" and obtained Sichuan Provincial Project. 
+    - In this project, I am mainly responsible for 50% of the content of the declaration writing work, 50% of the literature collation work, 55% of the data analysis work and 90% of the project process promotion work.
     """)
     
     st.markdown("---") 
